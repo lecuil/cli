44c514f7f94c003bd22873f3f3009fd96d21e8fa
SJYI <13183817938@163.com>
2025-06-17 11:17:08 +0800

    feat: 对目录进行拆包与移动位置

---
+++ core/cli/bin/index.js
@@ -0,0 +1,14 @@
+#! /usr/bin/env node
+
+import importLocal from 'import-local'
+import npmlog from 'npmlog'
+import main from '../lib/index.js'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+
+if (importLocal(__filename)) {
+  npmlog.info('cli', '使用 cli 本地版')
+} else {
+  main(process.argv.slice(2))
+}
