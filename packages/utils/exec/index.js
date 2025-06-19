3b66440168fb3882bb1701a891fd25d86748be94
SJYI <13183817938@163.com>
2025-06-19 16:13:34 +0800

    feat(package-utils): Package构造函数与基础属性

--- packages/utils/exec/index.js
+++ packages/utils/exec/index.js
@@ -1,9 +1,17 @@
+import log from '../log/index.js'
 import { Package } from '../package/index.js'
 
 export const exec = () => {
-  const pkg = new Package()
+  const targetPath = process.env.CLI_TARGET_PATH
+  const storePath = process.env.CLI_HOME_PATH
+  log.verbose('targetPath', targetPath)
+  log.verbose('storePath', storePath)
+
+  const pkg = new Package({
+    targetPath,
+    storePath,
+    name: '@lecuil-cli/init',
+    version: 'latest',
+  })
   console.log(pkg, 'pkg')
-  console.log('exec')
-  console.log(process.env.CLI_TARGET_PATH)
-  console.log(process.env.CLI_HOME_PATH)
 }
--- packages/utils/package/index.js
+++ packages/utils/package/index.js
@@ -1,5 +1,56 @@
+import { isObject } from '../data/index.js'
+
 export class Package {
-  constructor() {
-    console.log('Package constructor')
+  /**
+   * Package路径
+   */
+  targetPath = ''
+
+  /**
+   * Package存储路径
+   */
+  storePath = ''
+
+  /**
+   * Package名称
+   */
+  name = ''
+
+  /**
+   * Package版本
+   */
+  version = ''
+
+  constructor(options) {
+    if (!options) {
+      throw new Error('Package构造函数参数不能为空')
+    }
+    if (!isObject(options)) {
+      throw new Error('Package构造函数参数必须为对象')
+    }
+    this.targetPath = options.targetPath
+    this.storePath = options.storePath
+    this.name = options.name
+    this.version = options.version
   }
+
+  /**
+   * 判断当前Package是否存在
+   */
+  exists() {}
+
+  /**
+   * 安装
+   */
+  install() {}
+
+  /**
+   * 更新
+   */
+  update() {}
+
+  /**
+   * 获取入口文件位置
+   */
+  getRootPath() {}
 }
