383450d8f208dbb4bb79d310202857517172aac7
SJYI <13183817938@163.com>
2025-06-20 14:12:33 +0800

    feat(core-cli): ts的正常使用与编译

--- commands/init/lib/index.js
+++ commands/init/lib/index.js
@@ -1,5 +1,3 @@
-'use strict'
-
 const init = (projectName, _, cmdObj) => {
   const options = cmdObj.parent.opts()
   console.log('init', projectName, options, process.env.CLI_TARGET_PATH)
--- core/cli/bin/index.ts
+++ core/cli/bin/index.ts
@@ -1,14 +1,15 @@
 #! /usr/bin/env node
 
+import { log } from '@lecuil-cli/utils'
 import importLocal from 'import-local'
-import npmlog from 'npmlog'
-import main from '../src/index.js'
 import { fileURLToPath } from 'url'
+import main from '../src'
 
 const __filename = fileURLToPath(import.meta.url)
 
 if (importLocal(__filename)) {
-  npmlog.info('cli', '使用 cli 本地版')
+  log.info('cli', '使用 cli 本地版')
 } else {
-  main(process.argv.slice(2))
+  // main(process.argv.slice(2))
+  main()
 }
