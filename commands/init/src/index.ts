fce0f3ac434a67d2bd068ac3a6bd88bd23978994
facil <13183817938@163.com>
2025-06-26 00:17:27 +0800

    feat(commands-init): 判断当前创建目录是否为空

--- commands/init/src/index.ts
+++ commands/init/src/index.ts
@@ -1,4 +1,5 @@
-import { Command, type CommandOptions } from '@lecuil-cli/utils'
+import { Command, log, type CommandOptions } from '@lecuil-cli/utils'
+import fs from 'fs'
 
 export class InitCommand extends Command {
   force: boolean = false
@@ -8,12 +9,35 @@ export class InitCommand extends Command {
   }
 
   exec(): void {
-    // 准备阶段
-    // 下载模板
-    // 安装模板
+    try {
+      // 准备阶段
+      this.prepare()
+
+      // 下载模板
+      // 安装模板
+    } catch (error) {
+      log.error('执行失败', (error as Error).message)
+    }
   }
 
-  prepare() {}
+  prepare() {
+    if (!this.isCwdEmpty()) {
+    }
+
+    // 判断是否强制更新
+    // 选择创建项目或文件
+    // 获取项目基本信息
+  }
+
+  /**
+   * 判断当前目录是否为空
+   * @returns
+   */
+  private isCwdEmpty() {
+    const localPath = process.cwd()
+    const fileList = fs.readdirSync(localPath)
+    return fileList && fileList.filter((file) => !file.startsWith('.') && ['node_modules'].includes(file)).length === 0
+  }
 }
 
 const init = (args: CommandOptions) => {
