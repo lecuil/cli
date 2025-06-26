5a27ae1ef3a007edf62423b56c3f9c0294a5915c
facil <13183817938@163.com>
2025-06-26 23:49:23 +0800

    feat(commands-init): 用户传入目录的清空确认操作

--- commands/init/src/index.ts
+++ commands/init/src/index.ts
@@ -1,5 +1,7 @@
 import { Command, log, type CommandOptions } from '@lecuil-cli/utils'
 import fs from 'fs'
+import { confirm } from '@inquirer/prompts'
+import fse from 'fs-extra'
 
 export class InitCommand extends Command {
   force: boolean = false
@@ -8,10 +10,10 @@ export class InitCommand extends Command {
     this.force = Boolean(this._options.force)
   }
 
-  exec(): void {
+  async exec() {
     try {
       // 准备阶段
-      this.prepare()
+      await this.prepare()
 
       // 下载模板
       // 安装模板
@@ -20,11 +22,30 @@ export class InitCommand extends Command {
     }
   }
 
-  prepare() {
-    if (!this.isCwdEmpty()) {
+  async prepare() {
+    const localPath = process.cwd()
+    if (!this.isDirEmpty(localPath)) {
+      let isContinue = false
+      if (!this.force) {
+        isContinue = await confirm({
+          message: '当前目录不为空，是否继续？',
+          default: false,
+        })
+        if (!isContinue) return
+      }
+
+      log.verbose('localPath', localPath)
+      if (isContinue || this.force) {
+        const confirmDelete = await confirm({
+          message: '是否确认清空当前目录？',
+          default: false,
+        })
+        if (confirmDelete) {
+          fse.emptyDirSync(localPath)
+        }
+      }
     }
 
-    // 判断是否强制更新
     // 选择创建项目或文件
     // 获取项目基本信息
   }
@@ -33,8 +54,7 @@ export class InitCommand extends Command {
    * 判断当前目录是否为空
    * @returns
    */
-  private isCwdEmpty() {
-    const localPath = process.cwd()
+  private isDirEmpty(localPath: string) {
     const fileList = fs.readdirSync(localPath)
     return fileList && fileList.filter((file) => !file.startsWith('.') && ['node_modules'].includes(file)).length === 0
   }
---
+++ commands/test/README.md
@@ -0,0 +1,11 @@
+# `@lecuil-cli/init`
+
+> TODO: description
+
+## Usage
+
+```
+const init = require('@lecuil-cli/init');
+
+// TODO: DEMONSTRATE API
+```
